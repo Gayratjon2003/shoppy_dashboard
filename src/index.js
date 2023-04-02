a0594edd1b0ddefce407d3b0653b1ff60722fb4c
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2023-04-02 18:31:38 +0500

    useContext added and sidebar created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { registerLicense } from '@syncfusion/ej2-base';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { registerLicense } from "@syncfusion/ej2-base";
+import { ContextProvider } from "./contexts/ContextProvider";
 // Registering Syncfusion license key
-registerLicense('ORg4AjUWIQA/Gnt2VVhkQlFacldJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkZiXH5ec3FVQ2NaVkQ=');
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+registerLicense(
+  "ORg4AjUWIQA/Gnt2VVhkQlFacldJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkZiXH5ec3FVQ2NaVkQ="
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
